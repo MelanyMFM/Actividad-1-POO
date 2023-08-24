7efb60c251af61a692c9026721580cc3df2ceaef
MelanyMFM <142820807+MelanyMFM@users.noreply.github.com>
2023-08-24 18:30:03 -0500

    Ejercicio 12 (Salario neto, bruto,...)

---
+++ Ejercicio12.java
@@ -0,0 +1,20 @@
+package ejercicio.pkg12;
+
+/**
+ * Un empleado trabaja 48 horas en la semana a razón de $5.000 hora. El porcentaje de
+retención en la fuente es del 12,5% del salario bruto. Se desea saber cuál es el salario bruto,
+la retención en la fuente y el salario neto del trabajador.
+ */
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        int horas_trabajadas = 48;
+        int salarioxhora = 5000;
+        int salario_bruto = horas_trabajadas * salarioxhora;
+        double retencion = salario_bruto * 0.125;
+        double salario_neto = salario_bruto - retencion;
+        System.out.println("Salario bruto: " + salario_bruto + "$");
+        System.out.println("Retencion en la fuente: " +(int)retencion+ "$");
+        System.out.println("Salario neto: " + (int)salario_neto + "$");
+    }
+    
+}
