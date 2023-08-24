117f2cd8d6182f7eea9a824c622a5e8ac4671579
MelanyMFM <142820807+MelanyMFM@users.noreply.github.com>
2023-08-23 19:26:05 -0500

    Ejercicio 5 (Prueba de escritorio SUMA)

---
+++ POOejercicio5.java
@@ -0,0 +1,26 @@
+package pooejercicio5;
+
+/**
+ *
+ * @author Melany
+ */
+public class POOejercicio5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double suma = 0;
+        int x = 20;
+        suma = suma + x;
+        int y = 40;
+        x = x + (y*y);
+        suma = suma + (double)x/y;
+        System.out.println(suma);
+    }
+    
+}
+
+
+
+
